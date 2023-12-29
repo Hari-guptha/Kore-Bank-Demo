e061528ff2734ebafdfdde32423be70507b3bb6c
Hari_Guptha <80567787+Hari-guptha@users.noreply.github.com>
2023-12-30 00:50:13 +0530

    1

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
                 <div style={{ display: "flex", gap: 20 }}>
                     <img style={{ width: "400px" }} src={Map} alt="Map" />
                     <div style={{ display: "flex", flexDirection: "column",marginLeft:"50px",width:"600px"}}>
-                        <TextField sx={{marginTop:"10%"}} id="outlined-basic" label="Email" placeholder='Enter your Email' variant="outlined" />
-                        <TextField sx={{marginTop:"10%"}} id="outlined-basic" label="Message" placeholder='Enter the message' variant="outlined" />
+                        <TextField sx={{marginTop:"10%",width:"100%"}} id="outlined-basic" label="Email" placeholder='Enter your Email' variant="outlined" />
+                        <TextField sx={{marginTop:"10%",width:"100%"}} id="outlined-basic" label="Message" placeholder='Enter the message' variant="outlined" />
                         <div style={{display:"flex",justifyContent:"center"}}>
                         <h4 style={{marginTop:"10%",textAlign:"center",width:"100px"}} id='btn'>Send</h4>
                         </div>
